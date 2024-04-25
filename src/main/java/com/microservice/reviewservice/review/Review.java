1b2951e52bbdd554060ec2f53028d01b4529ea46
Punit Sakre <punitsakre786@gmail.com>
2024-04-25 18:57:37 +0530

    creating-review-service

---
+++ src/main/java/com/microservice/reviewservice/review/Review.java
@@ -0,0 +1,63 @@
+package com.microservice.reviewservice.review;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.jobapp.company.Company;
+import jakarta.persistence.*;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Double rating;
+
+    @JsonIgnore
+    @ManyToOne
+    private Company company;
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
